a4fc09eaf4eb0a87c16699cc3c37a3ec61244818
Jeevan Gulab Pawar <jeevan.wavetechline@gmail.com>
2013-06-19 10:54:01 +0530

    jeevana

--- admin/config.inc.php
+++ admin/config.inc.php
@@ -2,8 +2,8 @@
 	$hostname = "localhost";
 	$database = "db";
 	$username = "root";
-	$password = "";
-    $mysql_link = mysql_connect("localhost", "root", "");   
+	$password = "bitnami";
+    $mysql_link = mysql_connect("localhost", "root", "bitnami");   
     mysql_select_db("ananya") or die("Could not select database");
     $images_dir = "photos";
 ?>
--- ananya.sql
+++ ananya.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 24, 2013 at 10:11 AM
+-- Generation Time: Jun 19, 2013 at 05:22 AM
 -- Server version: 5.5.24-log
 -- PHP Version: 5.3.13
 
@@ -40,8 +40,8 @@ CREATE TABLE IF NOT EXISTS `beauty` (
 --
 
 INSERT INTO `beauty` (`b_id`, `b_name`, `b_des1`, `b_des2`, `b_des3`) VALUES
-(1, 'Hairs', 'With our voluminous experience we have developed a well-knitted network that is spread in every nook and corner of the world. Some of the markets we deal with are East Europe, East Asia, Central America, North Europe, Middle East, South America, South/West Europe, South East Asia, North America, A', 'With our voluminous experience we have developed a well-knitted network that is spread in every nook and corner of the world. Some of the markets we deal with are East Europe, East Asia, Central America, North Europe, Middle East, South America, South/West Europe, South East Asia, North America, Australia/NZ, Indian Subcontinent, Caribbean, East/Middle Africa, South/West Africa, North Africa and other regions round the globe.With our voluminous experience we have developed a well-knitted network that is spread in every nook and corner of the world. Some of the markets we deal with are East Europe, East Asia, Central America, North Europe, Middle East, South America, South/West Europe, South East Asia, North America, Australia/NZ, Indian Subcontinent, Caribbean, East/Middle Africa, South/West Africa, North Africa and other regions round the globe.With our voluminous experience we have developed a well-knitted network that is spread in every nook and corner of the world. Some of the mark', 'e Africa, South/West Africa, North Africa and other regions round the globe.'),
-(2, 'eyes', 'With our voluminous experience we have developed a well-knitted network that is spread in every nook and corner of the world. Some of the markets we deal with are East Europe, East Asia, Central America, North Europe, Middle East, South America, South/West Europe, South East Asia, North America, Australia/NZ, Indian Subcontinent, Caribbean, East/Middle Africa, South/West Africa, North Africa and other regions round the globe.', 'With our voluminous experience we have developed a well-knitted network that is spread in every nook and corner of the world. Some of the markets we deal with are East Europe, East Asia, Central America, North Europe, Middle East, South America, South/West Europe, South East Asia, North America, Australia/NZ, Indian Subcontinent, Caribbean, East/Middle Africa, South/West Africa, North Africa and other regions round the globe.', ''),
+(1, 'Hairs', 'With our voluminous experience we have developed a well-knitted network that is spread in every nook and corner of the world. Some of the markets we deal with are East Europe, East Asia, Central America, North Europe, Middle East, South America, South/West Europe, South East Asia, North America, Australia/NZ, Indian Subcontinent, Caribbean, East/Middle Africa, South/West Africa, North Africa and other regions round the globe.With our voluminous experience we have developed a well-knitted network that is spread in every nook and corner of the world. Some of the markets we deal with are East Europe, East Asia, Central America, North Europe, Middle East, South America, South/West Europe, South East Asia, North America, Australia/NZ, Indian Subcontinent, Caribbean, East/Middle Africa, South/West Africa, North Africa and other regions round the globe.With our voluminous experience we have developed a well-knitted network that is spread in every nook and corner of the world. Some of the mark', 'With our voluminous experience we have developed a well-knitted network that is spread in every nook and corner of the world. Some of the markets we deal with are East Europe, East Asia, Central America, North Europe, Middle East, South America, South/West Europe, South East Asia, North America, Australia/NZ, Indian Subcontinent, Caribbean, East/Middle Africa, South/West Africa, North Africa and other regions round the globe.With our voluminous experience we have developed a well-knitted network that is spread in every nook and corner of the world. Some of the markets we deal with are East Europe, East Asia, Central America, North Europe, Middle East, South America, South/West Europe, South East Asia, North America, Australia/NZ, Indian Subcontinent, Caribbean, East/Middle Africa, South/West Africa, North Africa and other regions round the globe.With our voluminous experience we have developed a well-knitted network that is spread in every nook and corner of the world. Some of the mark', 'With our voluminous experience we have developed a well-knitted network that is spread in every nook and corner of the world. Some of the markets we deal with are East Europe, East Asia, Central America, North Europe, Middle East, South America, South/West Europe, South East Asia, North America, Australia/NZ, Indian Subcontinent, Caribbean, East/Middle Africa, South/West Africa, North Africa and other regions round the globe.With our voluminous experience we have developed a well-knitted network that is spread in every nook and corner of the world. Some of the markets we deal with are East Europe, East Asia, Central America, North Europe, Middle East, South America, South/West Europe, South East Asia, North America, Australia/NZ, Indian Subcontinent, Caribbean, East/Middle Africa, South/West Africa, North Africa and other regions round the globe.With our voluminous experience we have developed a well-knitted network that is spread in every nook and corner of the world. Some of the mark'),
+(2, 'Eyes', 'With our voluminous experience we have developed a well-knitted network that is spread in every nook and corner of the world. Some of the markets we deal with are East Europe, East Asia, Central America, North Europe, Middle East, South America, South/West Europe, South East Asia, North America, Australia/NZ, Indian Subcontinent, Caribbean, East/Middle Africa, South/West Africa, North Africa and other regions round the globe.', 'With our voluminous experience we have developed a well-knitted network that is spread in every nook and corner of the world. Some of the markets we deal with are East Europe, East Asia, Central America, North Europe, Middle East, South America, South/West Europe, South East Asia, North America, Australia/NZ, Indian Subcontinent, Caribbean, East/Middle Africa, South/West Africa, North Africa and other regions round the globe.', 'With our voluminous experience we have developed a well-knitted network that is spread in every nook and corner of the world. Some of the markets we deal with are East Europe, East Asia, Central America, North Europe, Middle East, South America, South/West Europe, South East Asia, North America, Australia/NZ, Indian Subcontinent, Caribbean, East/Middle Africa, South/West Africa, North Africa and other regions round the globe.'),
 (3, 'Nose', 'this', 'this', 'this'),
 (4, 'Lips', 'With our voluminous experience we have developed a well-knitted network that is spread in every nook and corner of the world. Some of the markets we deal with are East Europe, East Asia, Central America, North Europe, Middle East, South America, South/West Europe, South East Asia, North America, Australia/NZ, Indian Subcontinent, Caribbean, East/Middle Africa, South/West Africa, North Africa and other regions round the globe.', 'With our voluminous experience we have developed a well-knitted network that is spread in every nook and corner of the world. Some of the markets we deal with are East Europe, East Asia, Central America, North Europe, Middle East, South America, South/West Europe, South East Asia, North America, Australia/NZ, Indian Subcontinent, Caribbean, East/Middle Africa, South/West Africa, North Africa and other regions round the globe.', 'With our voluminous experience we have developed a well-knitted network that is spread in every nook and corner of the world. Some of the markets we deal with are East Europe, East Asia, Central America, North Europe, Middle East, South America, South/West Europe, South East Asia, North America, Australia/NZ, Indian Subcontinent, Caribbean, East/Middle Africa, South/West Africa, North Africa and other regions round the globe.'),
 (5, 'Face', 'thsi', 'this', 'thsi'),
@@ -50,6 +50,28 @@ INSERT INTO `beauty` (`b_id`, `b_name`, `b_des1`, `b_des2`, `b_des3`) VALUES
 (8, 'Nails', 'this', 'this', 'this'),
 (9, 'Skin', 'this', 'this', 'this'),
 (10, 'Foot', 'this', 'this', 'thi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `f_product`
+--
+
+CREATE TABLE IF NOT EXISTS `f_product` (
+  `f_id` int(11) NOT NULL AUTO_INCREMENT,
+  `f_image` varchar(25) NOT NULL,
+  `f_name` varchar(25) NOT NULL,
+  `f_category` varchar(25) NOT NULL,
+  `f_type` varchar(25) NOT NULL,
+  PRIMARY KEY (`f_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=24 ;
+
+--
+-- Dumping data for table `f_product`
+--
+
+INSERT INTO `f_product` (`f_id`, `f_image`, `f_name`, `f_category`, `f_type`) VALUES
+(23, '23.jpg', 'Face', 'Product', 'None');
 
 -- --------------------------------------------------------
 
@@ -65,6 +87,29 @@ CREATE TABLE IF NOT EXISTS `highlight` (
   `h_pho` varchar(25) NOT NULL,
   PRIMARY KEY (`h_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `slider`
+--
+
+CREATE TABLE IF NOT EXISTS `slider` (
+  `p_id` int(11) NOT NULL AUTO_INCREMENT,
+  `p_name` varchar(25) NOT NULL,
+  PRIMARY KEY (`p_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33 ;
+
+--
+-- Dumping data for table `slider`
+--
+
+INSERT INTO `slider` (`p_id`, `p_name`) VALUES
+(27, '27.jpg'),
+(28, '28.jpg'),
+(29, '29.jpg'),
+(30, '30.jpg'),
+(32, '32.jpg');
 
 -- --------------------------------------------------------
 
