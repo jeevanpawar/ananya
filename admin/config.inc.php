a7b2aeaaf3d386ec058f8e679d876707a8fb97b7
Jeevan Gulab Pawar <jeevan.wavetechline@gmail.com>
2013-06-19 11:05:00 +0530

    jeevandb

--- admin/config.inc.php
+++ admin/config.inc.php
@@ -4,6 +4,7 @@
 	$username = "root";
 	$password = "bitnami";
     $mysql_link = mysql_connect("localhost", "root", "bitnami");   
-    mysql_select_db("ananya") or die("Could not select database");
-    $images_dir = "photos";
+    mysql_select_db("db") or die("Could not select database");
+	$images_dir = "photos";
 ?>
+r
